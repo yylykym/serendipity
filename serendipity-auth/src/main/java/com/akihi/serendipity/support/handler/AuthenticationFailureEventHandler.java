aaabea9206f536a92bb78f6d2928c1d49f9a492b
tty <tty@gmail.com>
2023-09-13 18:14:41 +0800

    1

---
+++ serendipity-auth/src/main/java/com/akihi/serendipity/support/handler/AuthenticationFailureEventHandler.java
@@ -0,0 +1,16 @@
+package com.akihi.serendipity.support.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class AuthenticationFailureEventHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+    }
+}
